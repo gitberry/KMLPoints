e9788e35afcd7f3e8d8f81c4bbfa34acb49b1934
nbDevW10b <nbDevW10b@NBW10DEVBOXB>
2022-01-25 16:29:19 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KMLPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is the KMLPoints CL utility!!");
+        }
+    }
+}
